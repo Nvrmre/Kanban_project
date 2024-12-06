39272da54118949b4518eb8678c85e4690ef7bf1
yoga233 <rg.gamiiing233@gmail.com>
2024-12-06 16:46:35 +0700

    benerin cache login

--- database/migrations/0001_01_01_000001_create_cache_table.php
+++ database/migrations/0001_01_01_000001_create_cache_table.php
@@ -10,19 +10,19 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-    {
-        Schema::create('cache', function (Blueprint $table) {
-            $table->string('key')->primary();
-            $table->mediumText('value');
-            $table->integer('expiration');
-        });
+{
+    Schema::create('cache', function (Blueprint $table) {
+        $table->string('key')->primary();
+        $table->mediumText('value');
+        $table->bigInteger('expiration');  // Ubah ke BIGINT
+    });
 
-        Schema::create('cache_locks', function (Blueprint $table) {
-            $table->string('key')->primary();
-            $table->string('owner');
-            $table->integer('expiration');
-        });
-    }
+    Schema::create('cache_locks', function (Blueprint $table) {
+        $table->string('key')->primary();
+        $table->string('owner');
+        $table->integer('expiration');
+    });
+}
 
     /**
      * Reverse the migrations.
